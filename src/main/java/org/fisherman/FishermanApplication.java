6091f513ef85bff601542c50a0dca2690d336824
Michael F Kelly Jr <michael@digital-transform.org>
2025-03-26 13:12:40 -0500

    fix main class

--- src/main/java/org/fisherman/FishermanApplication.java
+++ src/main/java/org/fisherman/FishermanApplication.java
@@ -1,1 +1,14 @@
-public class FishermanApplication {}
+package org.fisherman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class FishermanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FishermanApplication.class, args);
+    }
+}
